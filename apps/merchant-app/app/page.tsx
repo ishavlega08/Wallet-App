52904b834a2af84607e4b4428c8c6415dd6b7d02
Ishav <ishavlega@gmail.com>
2024-06-29 17:58:10 +0530

    init

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useBalance } from "@repo/store/useBalance"
+
+export default function Home() {
+  const balance = useBalance();
+
+  return (
+    <div>
+      hi there {balance}
+    </div>
+  );
+}
---
+++ apps/user-app/app/api/user/route.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@repo/db/client";
+import { NextResponse } from "next/server";
+
+const client = new PrismaClient();
+
+export const GET = async () => {
+    await client.user.create({
+        data: {
+            email: "asd",
+            name: "asdf"
+        }
+    })
+
+    return NextResponse.json({
+        message: "hi there"
+    })
+}
