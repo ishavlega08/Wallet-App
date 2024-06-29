4e08b1620e0d5687d812e53ada752cdd02efdf49
Ishav <ishavlega@gmail.com>
2024-06-29 19:50:40 +0530

    bootstrapping done

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
-import { useBalance } from "@repo/store/useBalance";
+import { Appbar } from "@repo/ui/appbar";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-  const balance = useBalance();
+  const session = useSession();
 
   return (
     <div>
-      hi there {balance}
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
     </div>
   );
 }
